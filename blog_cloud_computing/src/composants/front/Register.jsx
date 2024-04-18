de318b944da15997549f975c8b7ea1c0d3dc3807
Fantik94 <84931090+Fantik94@users.noreply.github.com>
2024-04-18 11:52:21 +0200

    connexion et register fini

--- blog_cloud_computing/src/composants/front/Register.jsx
+++ blog_cloud_computing/src/composants/front/Register.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 function Register() {
     const [formData, setFormData] = useState({
         email: '',
         password: '',
-        role: 'USER'  
+        role: 'USER'  // Assumant que vous souhaitez toujours collecter un rôle lors de l'inscription.
     });
 
     const handleChange = (e) => {
@@ -23,20 +24,63 @@ function Register() {
     };
 
     return (
-        <div className="form-container">
-            <div className="form-box">
-                <h2 className="form-header">Inscription</h2>
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        Email:
-                        <input type="email" name="email" value={formData.email} onChange={handleChange} required />
-                    </label>
-                    <label>
-                        Mot de passe:
-                        <input type="password" name="password" value={formData.password} onChange={handleChange} required />
-                    </label>
-                    <button type="submit">S'inscrire</button>
+        <div className="flex items-center justify-center px-6 py-12">
+            <div className="max-w-md w-full space-y-8">
+                <div>
+                    <img
+                        className="mx-auto h-12 w-auto"
+                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                        alt="Workflow"
+                    />
+                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+Création du compte                    </h2>
+                </div>
+                <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+                    <input type="hidden" name="remember" value="true" />
+                    <div className="rounded-md shadow-sm -space-y-px">
+                        <div>
+                            <label htmlFor="email" className="sr-only">Email </label>
+                            <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                autoComplete="email"
+                                required
+                                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                placeholder="Email address"
+                                value={formData.email}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="password" className="sr-only">Password</label>
+                            <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                autoComplete="current-password"
+                                required
+                                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                placeholder="Password"
+                                value={formData.password}
+                                onChange={handleChange}
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <button
+                            type="submit"
+                            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        >
+                            Register
+                        </button>
+                    </div>
                 </form>
+                <div className="text-sm text-center">
+                    <Link to="/login" className="font-medium text-indigo-600 hover:text-indigo-500">
+Vous avez déjà un compte ? Connectez vous ici                    </Link>
+                </div>
             </div>
         </div>
     );
