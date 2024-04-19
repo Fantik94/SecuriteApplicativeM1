3016044be0b3c03635da3c7363314f503af02054
Fantik94 <84931090+Fantik94@users.noreply.github.com>
2024-04-19 11:25:57 +0200

    pepe

--- blog_cloud_computing/src/composants/front/Register.jsx
+++ blog_cloud_computing/src/composants/front/Register.jsx
@@ -30,9 +30,9 @@ function Register() {
             <div className="max-w-md w-full space-y-8">
                 <div>
                     <img
-                        className="mx-auto h-12 w-auto"
-                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                        alt="Workflow"
+                        className="mx-auto h-14 w-auto"
+                        src="../../pepe.jpg"
+                        alt="pepe"
                     />
                     <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
                         Création du compte
